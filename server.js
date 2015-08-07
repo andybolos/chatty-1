5c0dd52298b8e70fafb354b151b345dfcefd3d22
Andy Bolos <andybolos@gmail.com>
2015-08-07 15:30:35 -0600

    Mini project

--- server.js
+++ server.js
@@ -1,52 +1,118 @@
 'use strict';
 
-// var messages = [{username: 'Philipp Schulte', message: 'This is a test!'}];
-var messages = [];
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
 
-var http = require('http');
+app.use(bodyParser.json());
 
-//process.argv is the list of arguments in node
-var port = process.argv[2] || 3000;
+var messages = ["DM5 Rocks!", "I can NODE", "IheartDEV"];
 
-http.createServer(function(req, res) {
-	switch (req.method) {
-		case 'GET':
-		    res.writeHead(200, {
-		      	'Connection': 'close',
-		      	'Content-Type': 'application/json',
-		     	'Access-Control-Allow-Origin': '*'
-		    });
-		    res.end(JSON.stringify(messages));
-			break;
-		case 'POST':
-	       	var postData = '';
-	       	req.on('data', function(chunk) {
-	        	postData += chunk.toString();
-	        });    
-		    req.on('end', function() {
-		      	//console.log(JSON.parse(postData));
-		      	var msg = JSON.parse(postData);
-		      	var timestamp = new Date();
-		      	msg.timestamp = timestamp;
-		      	messages.push(msg);
-		     	res.writeHead(200, {
-		        	'Connection': 'close',
-		       		'Content-Type': 'application/json',
-		        	'Access-Control-Allow-Origin': '*'
-		    	});
-		    	res.end(JSON.stringify(messages));
-		    });
-			break;
-		case 'OPTIONS': 
-			res.writeHead(200, {
-	  			'Connection': 'close',
-	  			'Content-Type': 'application/json',
-	 			'Access-Control-Allow-Origin': '*',
-	 			'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
-	  			'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
-			});
-			res.end();
-			break;
-	}
+app.get('/', function(request, response) {
+	response.send(JSON.stringify(messages));
+});
 
-}).listen(port);
+app.post('/', function(request, response) {
+	var newMsg = request.body.message;
+	messages.push(newMsg);
+	response.send(JSON.stringify(messages));
+});
+
+app.put('/api/person/:id', function(req, res) {
+	var ourId = req.params.id;
+	res.send("Your id is " + ourId);
+});
+
+
+
+
+app.listen(8080);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // var messages = [{username: 'Philipp Schulte', message: 'This is a test!'}];
+// var messages = [];
+
+// var http = require('http');
+
+// //process.argv is the list of arguments in node
+// var port = process.argv[2] || 3000;
+
+// http.createServer(function(req, res) {
+// 	switch (req.method) {
+// 		case 'GET':
+// 		    res.writeHead(200, {
+// 		      	'Connection': 'close',
+// 		      	'Content-Type': 'application/json',
+// 		     	'Access-Control-Allow-Origin': '*'
+// 		    });
+// 		    res.end(JSON.stringify(messages));
+// 			break;
+// 		case 'POST':
+// 	       	var postData = '';
+// 	       	req.on('data', function(chunk) {
+// 	        	postData += chunk.toString();
+// 	        });
+// 		    req.on('end', function() {
+// 		      	//console.log(JSON.parse(postData));
+// 		      	var msg = JSON.parse(postData);
+// 		      	var timestamp = new Date();
+// 		      	msg.timestamp = timestamp;
+// 		      	messages.push(msg);
+// 		     	res.writeHead(200, {
+// 		        	'Connection': 'close',
+// 		       		'Content-Type': 'application/json',
+// 		        	'Access-Control-Allow-Origin': '*'
+// 		    	});
+// 		    	res.end(JSON.stringify(messages));
+// 		    });
+// 			break;
+// 		case 'OPTIONS':
+// 			res.writeHead(200, {
+// 	  			'Connection': 'close',
+// 	  			'Content-Type': 'application/json',
+// 	 			'Access-Control-Allow-Origin': '*',
+// 	 			'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+// 	  			'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+// 			});
+// 			res.end();
+// 			break;
+// 	}
+
+// }).listen(port);
